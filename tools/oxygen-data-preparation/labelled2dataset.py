51c4a6df75a1a1d2e52b3ad8111a874547198d67
thanit456 <too_w_o@hotmail.com>
2020-09-21 02:40:51 +0700

    add num distractors option excluding them from test set

--- tools/oxygen-data-preparation/labelled2dataset.py
+++ tools/oxygen-data-preparation/labelled2dataset.py
@@ -3,8 +3,14 @@ import shutil
 import numpy as np
 from sklearn.model_selection import train_test_split
 from tqdm import tqdm 
+import random 
 
 from utils import create_dir
+
+seed = 1024
+np.random.seed(seed)
+random.seed(seed)
+
 # Dataset_without_eth 
 #   - date
 #        [N] [gender]_[age]_[id]/[gender]_[age]_[camid]_[date_timestamp]_[anonymous].jpg
@@ -12,6 +18,7 @@ from utils import create_dir
 
 
 # labelled dataset to labelled-pool
+
 src_dir_path = './mock_Dataset_without_eth'
 dst_dir_path = './Oxygen1'
 train_dir_path = dst_dir_path + '/bounding_box_train'
@@ -20,28 +27,43 @@ query_dir_path = dst_dir_path + '/query'
 create_dir(query_dir_path)
 gallery_dir_path = dst_dir_path + '/bounding_box_test'
 create_dir(gallery_dir_path)
-hasDistractor = False
+hasDistractor = True
+if hasDistractor:
+    distractor_dir_path = dst_dir_path + '/distractor'
+    create_dir(distractor_dir_path)
+num_distractor = 10
 
 org_img_paths = list()
 processed_file_names = list()
 path2newname_mapper = dict()
+
+num_people = len(os.listdir(src_dir_path))
+distractor_pids = list(range(1, num_people+1))
+random.shuffle(distractor_pids)
+distractor_pids = distractor_pids[:num_distractor]
+distractor_uniques = [e-1 for e in sorted(distractor_pids)]
+distractor_pairs = list()
 
 for root, subdirs, file_names in os.walk(src_dir_path):
     if len(file_names) != 0:
         pid = root[root.rfind('/')+1:]
         for file_name in file_names:
             if file_name[0] == '.': continue
-            gender, age, camid, timestamp, uniqueid = file_name.split('_')
+            gender, age, camid, timestamp, uniqueid = file_name.split('_')                
             # ! uniqueid (unique_id + '.jpg')
-            path2newname_mapper[root + '/' + file_name] = pid + '_' + camid + '_' + uniqueid
-
+            if hasDistractor: 
+                if int(pid) in distractor_pids:
+                    distractor_pairs.append([root + '/' + file_name, pid + '_' + camid + '_' + uniqueid])
+                else:
+                    path2newname_mapper[root + '/' + file_name] = pid + '_' + camid + '_' + uniqueid
+            else:
+                path2newname_mapper[root + '/' + file_name] = pid + '_' + camid + '_' + uniqueid
+                
 # split train, test, distractor 
 # stratify between train and test
-seed = 1024
 labels = [int(file_name.split('_')[0]) - 1  for file_name in path2newname_mapper.values()]
-num_people = len(np.unique(np.array(labels)))
-print('The number of ids : ', num_people)
 
+print('The number of ids : ', num_people)
 print(len(list(path2newname_mapper.keys())))
 print('-' * 30)
 print(len(labels))
@@ -57,7 +79,6 @@ print('train unqiue id :', len(train_uniques))
 print('gallery unqiue id :', len(gallery_uniques))
 print('query unique id : ', len(query_uniques))
 
-print(list(train_uniques))
 with open(dst_dir_path + '/' + 'train_uniques.txt', 'w') as f:
     for i in list(train_uniques):
         f.write(str(i) + '\n')
@@ -67,6 +88,10 @@ with open(dst_dir_path + '/' + 'gallery_uniques.txt', 'w') as f:
 with open(dst_dir_path + '/' + 'query_uniques.txt', 'w') as f:
     for i in list(query_uniques):
         f.write(str(i) + '\n')
+if hasDistractor:
+    with open(dst_dir_path + '/' + 'distractor_uniques.txt', 'w') as f:
+        for i in distractor_uniques:
+            f.write(str(i) + '\n')
 
 for img_path, file_name in tqdm(train_pairs):
     shutil.copyfile(img_path, train_dir_path + '/' + file_name)
@@ -74,3 +99,6 @@ for img_path, file_name in tqdm(gallery_pairs):
     shutil.copyfile(img_path, gallery_dir_path + '/' + file_name)
 for img_path, file_name in tqdm(query_pairs):
     shutil.copyfile(img_path, query_dir_path + '/' + file_name)
+if hasDistractor:
+    for img_path, file_name in tqdm(distractor_pairs):
+        shutil.copyfile(img_path, distractor_dir_path + '/' + file_name)
