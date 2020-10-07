b2fa8d19ed45f60c67ae495b1f34b44e8de28c92
Thanit Tativannarat <too_w_o@hotmail.com>
2020-10-07 17:30:46 +0700

    add ArcFace implementation

--- modeling/layer/cosine_loss.py
+++ modeling/layer/cosine_loss.py
@@ -4,6 +4,66 @@ import torch.nn.functional as F
 from torch.nn import Parameter
 import math
 
+class ArcFace(nn.Module):
+    def __init__(self, num_features, num_classes, s=30.0, m=0.50):
+        super(ArcFace, self).__init__()
+        self.num_features = num_features
+        self.num_classes = num_classes
+        self.s = s
+        self.m = m
+        self.W = Parameter(torch.FloatTensor(num_classes, num_features))
+        nn.init.xavier_uniform_(self.W)
+
+    def forward(self, input, label=None):
+        # normalized features
+        x = F.normalize(self.x)
+        # normalized weights 
+        W = F.normalize(self.W)
+        # dot product
+        logits = F.linear(x, W)
+        if label is None:
+            return logits 
+        # add margin 
+        theta = torch.acoss(torch.clamp(logits, -1.0 + 1e-7, 1.0 - 1e-7))
+        target_logits = torch.cos(theta + self.m)
+        one_hot = torch.zeros_like(logits)
+        one_hot.scatter_(1, label.view(-1, 1).long(), 1)
+        output = logits * (1 - one_hot) + target_logits * one_hot
+        # feature re-scale 
+        output *= self.s
+        
+        return output
+
+# class ArcCos(nn.Module):
+#     def __init__(self, in_features, out_features, s=30.0, m=0.50, bias=False)
+#         super(ArcCos, self).__init__()
+#         self.in_features = in_features
+#         self.out_features = out_features
+#         self.s = s
+#         self.m = m
+#         self.cos_m = math.cos(m)
+#         self.sin_m = math.sin(m)
+
+#         self.th = math.cos(math.pi - m)
+#         self.mm = math.sin(math.pi - m) * m
+        
+#         self.weight = Parameter(torch.Tensor(out_features, in_features))
+#         if bias:
+#             self.bias = Parameter(torch.Tensor(out_features))
+#         else:
+#             self.register_parameter('bias', None)
+#         self.register_parameters()
+
+#     def reset_parameters(self):
+#         nn.init.kaiming_uniform_(self.weight, a=math.sqrt(5))
+#         if self.bias is not None:
+#             fan_in, _ = nn.init._calculate_fan_in_and_fan_out(self.weight)
+#             boud = 1 / math.sqrt(fan_in)
+#             nn.init.uniform_(self.bias, -bound, bound)
+        
+#     def forward(self, input, label):
+#         # fix for
+
 class AdaCos(nn.Module):
     def __init__(self, num_features, num_classes, m=0.50):
         super(AdaCos,self).__init__()
