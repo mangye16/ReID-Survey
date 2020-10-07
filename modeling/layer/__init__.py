19fac836d7aa0d4cff7796480ec19590dcd34675
Thanit Tativannarat <too_w_o@hotmail.com>
2020-10-07 17:37:01 +0700

    fix import of pooling.py

--- modeling/layer/__init__.py
+++ modeling/layer/__init__.py
@@ -3,4 +3,4 @@
 from .center_loss import CenterLoss
 from .triplet_loss import CrossEntropyLabelSmooth, TripletLoss, WeightedRegularizedTriplet
 from .non_local import Non_local
-from .gem_pool import GeneralizedMeanPooling, GeneralizedMeanPoolingP
+from .pooling import GeneralizedMeanPooling, GeneralizedMeanPoolingP
