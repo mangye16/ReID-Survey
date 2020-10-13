94fa06fbeaab6c6590882c5b4b20f904f720b8c4
Thanit Tativannarat <too_w_o@hotmail.com>
2020-10-13 23:50:34 +0700

    create a build_loss

--- losses/__init__.py
+++ losses/__init__.py
@@ -1,2 +1,33 @@
 import torch 
+import torch.nn as nn
+
 from .id_loss import CrossEntropyLabelSmooth
+from .center_loss import CenterLoss
+from .circle_loss import CircleLoss
+from .cosine_loss import CosFace, AdaCos, ArcFace
+from .triplet_loss import TripletLoss, WeightedRegularizedTriplet
+from .smooth_ap_loss import SmoothAP
+
+def build_loss_fn(cfg, num_classes):
+    if cfg.MODEL.ID_LOSS_TYPE == 'none':
+        def id_loss_fn(score, target):
+            return 0
+    else:
+        id_loss_fn = CrossEntropyLabelSmooth(num_classes=num_classes)
+    
+    if cfg.MODEL.METRIC_LOSS_TYPE == 'triplet':
+        metric_loss_fn = TripletLoss(margin=0.0)
+    elif cfg.MODEL.METRIC_LOSS_TYPE == 'circle':
+        metric_loss_fn = CircleLoss(m=cfg.MODEL.METRIC_LOSS.MARGIN, s=cfg.MODEL.METRIC_LOSS.SCALE)
+    elif cfg.MODEL.METRIC_LOSS_TYPE == 'smoothAP':
+        assert(cfg.SOLVER.BATCH_SIZE % cfg.DATALOADER.NUM_INSTANCE == 0)
+        metric_loss_fn = SmoothAP(anneal=0.01, batch_size=cfg.SOLVER.BATCH_SIZE, 
+                                num_id=cfg.SOLVER.BATCH_SIZE // cfg.DATALOADER.NUM_INSTANCE, feat_dims=2048
+        )
+    else:
+        def metric_loss_fn(feat, target, feat_t, target_t):
+            return 0
+    def loss_func(score, feat, target, feat_t, target_t):
+        return id_loss_fn(score, target), metric_loss_fn(feat, target, feat_t, target_t)
+        
+    return loss_func
--- losses/triplet_loss.py
+++ losses/triplet_loss.py
@@ -111,37 +111,6 @@ class TripletLoss(object):
             loss = self.ranking_loss(dist_an - dist_ap, y)
         return loss, dist_ap, dist_an
 
-class CrossEntropyLabelSmooth(nn.Module):
-    """Cross entropy loss with label smoothing regularizer.
-
-    Reference:
-    Szegedy et al. Rethinking the Inception Architecture for Computer Vision. CVPR 2016.
-    Equation: y = (1 - epsilon) * y + epsilon / K.
-
-    Args:
-        num_classes (int): number of classes.
-        epsilon (float): weight.
-    """
-    def __init__(self, num_classes, epsilon=0.1, use_gpu=True):
-        super(CrossEntropyLabelSmooth, self).__init__()
-        self.num_classes = num_classes
-        self.epsilon = epsilon
-        self.use_gpu = use_gpu
-        self.logsoftmax = nn.LogSoftmax(dim=1)
-
-    def forward(self, inputs, targets):
-        """
-        Args:
-            inputs: prediction matrix (before softmax) with shape (batch_size, num_classes)
-            targets: ground truth labels with shape (num_classes)
-        """
-        log_probs = self.logsoftmax(inputs)
-        targets = torch.zeros(log_probs.size()).scatter_(1, targets.unsqueeze(1).data.cpu(), 1)
-        if self.use_gpu: targets = targets.cuda()
-        targets = (1 - self.epsilon) * targets + self.epsilon / self.num_classes
-        loss = (- targets * log_probs).mean(0).sum()
-        return loss
-
 
 def softmax_weights(dist, mask):
     max_v = torch.max(dist * mask, dim=1, keepdim=True)[0]
